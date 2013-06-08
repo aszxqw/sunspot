84313a092b4ce90e54b68986615cbaeeee50ad2b
wyy <wuyanyi09@gmail.com>
2013-06-08 22:52:34 +0800

    add counter_interface.py

---
+++ counter_interface.py
@@ -0,0 +1,9 @@
+class CounterInterface:
+    def __init__(self):
+        pass
+
+    def call_counter(self):
+        assert False,'the subclasss must define call_counter function'
+
+    def call_storer(self):
+        assert False,'the subclasss must define call_storer function'
